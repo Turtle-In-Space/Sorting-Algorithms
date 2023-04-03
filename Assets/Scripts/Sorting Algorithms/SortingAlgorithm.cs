be891380962fc6ecda08cbe9e298e13be6716e9a
Turtle-In-Space <elias.svensson63@gmail.com>
2023-04-03 19:09:25 +0200

    BubbleSort

---
+++ Assets/Scripts/Sorting Algorithms/SortingAlgorithm.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingAlgorithm : MonoBehaviour
+{
+    public virtual int[] Sort(int[] array)
+    {
+        print("oopos");
+        return null;
+    }
+}
