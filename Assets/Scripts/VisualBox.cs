d6c261e4636f84f50efbf1636a44ba61047654fd
Turtle-In-Space <elias.svensson63@gmail.com>
2023-04-17 16:41:40 +0200

    added switch function, made pillar height correspond to element size in given array

--- Assets/Scripts/VisualBox.cs
+++ Assets/Scripts/VisualBox.cs
@@ -9,6 +9,7 @@ public class VisualBox : MonoBehaviour
     [SerializeField] private GameObject pillarPrefab;
 
     private Transform pillarParent;
+    private GameObject[] pillars;
 
     private float boxWidth = 570f;
     private float maxPillarHeight = 210f;
@@ -16,7 +17,7 @@ public class VisualBox : MonoBehaviour
 
     private void Awake()
     {
-        instance = this;
+        instance = this;        
     }
 
     private void Start()
@@ -26,6 +27,7 @@ public class VisualBox : MonoBehaviour
 
     public void DrawPillars(int[] array)
     {
+        pillars = new GameObject[array.Length];
         ClearBox();
 
         float pillarWidth = boxWidth / array.Length;
@@ -33,16 +35,22 @@ public class VisualBox : MonoBehaviour
         for (int i = 0; i < array.Length; i++)
         {
             Vector3 position = new(pillarWidth * i + pillarWidth/2, 0, 0);
-            float pillarHeight = maxPillarHeight - (maxPillarHeight * (array.Length - 1 - i) / array.Length);
+
+            float pillarHeight = maxPillarHeight - (maxPillarHeight * (array.Length - array[i]) / array.Length);
 
             GameObject pillar = Instantiate(pillarPrefab, pillarParent.TransformPoint(position), Quaternion.identity, pillarParent);            
             pillar.GetComponent<Pillar>().SetSize(pillarWidth, pillarHeight);
+            pillars[i] = pillar;
         }
     }
 
     public void SwapPillars(int i, int j)
     {
+        Vector3 positionI = pillars[i].transform.position;
+        Vector3 positionJ = pillars[j].transform.position;
 
+        pillars[i].transform.position = pillarParent.TransformPoint(pillars[i].transform.position = new(positionJ.x, positionI.y));
+        pillars[j].transform.position = pillarParent.TransformPoint(new(positionI.x, positionJ.y));
     }
 
     private void ClearBox()
