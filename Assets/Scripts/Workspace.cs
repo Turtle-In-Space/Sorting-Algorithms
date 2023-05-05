85bf5493289354bbbe04549f8d7e1e2d3998817c
Turtle-In-Space <elias.svensson63@gmail.com>
2023-05-05 09:55:48 +0200

    Visual change, Changed slider to input

--- Assets/Scripts/Workspace.cs
+++ Assets/Scripts/Workspace.cs
@@ -1,75 +1,60 @@
 using System;
 using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
-
-
-public enum Algorithms
-{
-    Invalid,
-    Bubble,
-    Quick,
-    Gnome,
-    Bogo
-}
 
 
 public class Workspace : MonoBehaviour
 {
-    [SerializeField] private TMP_Dropdown algorithmSelect;
-    [SerializeField] private Slider slider;
-    [SerializeField] private TextMeshProUGUI elementNumber;
-    [SerializeField] private TextMeshProUGUI sortTime;
+    public static Workspace instance;
 
-    private SortingAlgorithm[] algorithms;
+    [SerializeField] private GameObject algorithmsParent;
+    [SerializeField] private TMP_Dropdown algorithmDropdown;
 
-    private int amountOfElements;
     private int[] array;
-
+    private int elementCount;
 
 
     private void Awake()
     {
-        algorithms = GetComponents<SortingAlgorithm>();
+        instance = this;
     }
 
     public void Run()
-    {                      
-        DateTime start = DateTime.Now;
-        algorithms[algorithmSelect.value - 1].Sort(array);
-        double time = (DateTime.Now - start).TotalMilliseconds;
-
-        VisualBox.instance.DrawPillars(array);
-
-        sortTime.text = "TIME: " + time + " ms";
-    }
-
-    public void OnValueChanged()
     {
-        amountOfElements = (int)slider.value;
-        elementNumber.text = slider.value.ToString();
-        array = InitArray(amountOfElements);
+        DropDownController.instance.GetSortingAlgorithm().Sort(array);
         VisualBox.instance.DrawPillars(array);
     }
 
-    private int[] InitArray(int elements)
+    public void UpdateElementCount(int value)
     {
-        int[] _array = new int[elements];
+        elementCount = value;
+        InitArray();
+        VisualBox.instance.DrawPillars(array);
+    }
 
-        for (int i = 0; i < elements; i++)
+    public void Shuffle()
+    {
+        for (int i = 0; i < elementCount; i++)
         {
-            _array[i] = i + 1;
+            int randPos = UnityEngine.Random.Range(1, elementCount - 1);
+            int temp = array[i];
+
+            array[i] = array[randPos];
+            array[randPos] = temp;
         }
 
-        for (int i = 0; i < elements; i++)
-        {
-            int randPos = UnityEngine.Random.Range(1, elements);
-            int temp = _array[i];
+        VisualBox.instance.DrawPillars(array);
+    }
 
-            _array[i] = _array[randPos];
-            _array[randPos] = temp;
+    private void InitArray()
+    {
+        array = new int[elementCount];
+
+        for (int i = 0; i < elementCount; i++)
+        {
+            array[i] = i + 1;
         }
 
-        return _array;
+        Shuffle();
     }
 }
